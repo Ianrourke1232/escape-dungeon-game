4ff654de302d3f2a4cc966755e76317020e15ea2
Ianrourke1232 <ianmunn2233@outlook.com>
2024-08-16 02:28:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let score = 0;
 
 const jumpHeight =20; // Height of the jump in pixels
 const jumpDuration = 1000; // Duration of the jump in milliseconds
-const obstacleSpeed = 20; // Speed of the obstacle movement
+const obstacleSpeed = 1; // Speed of the obstacle movement
 
 // Function to make the player jump
 function jump() {
